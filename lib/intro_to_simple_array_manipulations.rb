87152838f3d892055466cdbadfcf7453920bf8ef
Lu Daley <daleyluis@gmail.com>
2019-01-19 22:19:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,4 +1,5 @@
 def using_push(array,element)
+<<<<<<< HEAD
     countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
     countries_in_western_africa.push("Niger")
 end
@@ -50,3 +51,21 @@ def using_delete_at(array,integer)
   array.delete_at(integer)
 end
 
+=======
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  countries_in_western_africa.push("Niger")
+end
+
+
+
+
+=begin
+  it "takes in two arguments, an array and a string and adds that string to the end of the array using the push method" do 
+    countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+    next_country = "Niger"
+      
+    updated_array = using_push(countries_in_western_africa, next_country)
+    expect(updated_array.last).to eq(next_country)
+    end
+=end
+>>>>>>> e60c6e8ce5f9258a53198a4dc9ad79f82a6cbcc5
